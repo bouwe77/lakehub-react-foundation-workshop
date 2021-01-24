5b1a3500c1d458ac95c3eb601c608a67dbc88f0a
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-01-24 21:38:02 +0100

    .

--- src/exercises/03-useState/03.js
+++ src/exercises/03-useState/03.js
@@ -12,7 +12,7 @@ This is still under construction...
      and decrements a counter by clicking the + and - buttons. */
 
 /* 🌟 [01] - IMPORTANT: Please note the numbering of the steps in this exercise
-      are not ordered from top to bottom! You can go to step [02] now ;) */
+      are not ordered from top to bottom! Good luck with finding step [02] now... 🔎 */
 
 export default function App() {
   const [counter, setCounter] = useState(0);
@@ -21,21 +21,21 @@ export default function App() {
     setCounter(counter + 1);
   }
 
-  /* 🌟 [02] - Implement a function called `decrement` here, which decrements the counter state. */
+  /* 🌟 [03] - Implement a function called `decrement` here, which decrements the counter state. */
 
   return (
     <>
       <h1>Exercise 3</h1>
       <div>
-        {/* 🌟 [03] - Add an `onClick` to the button below and call the `decrement` function. */}
-        {/* 🌟 [04] - We do not allow negative numbers, so disable the decrement button when `counter` is 0. */}
+        {/* 🌟 [04] - Add an `onClick` to the button below and call the `decrement` function. */}
+        {/* 🌟 [05] - We do not allow negative numbers, so disable the decrement button when `counter` is 0. */}
         <button className={styles.btn}>-</button>
         <span className={styles.counter}>{counter}</span>
-        {/* 🌟 [01] - Add an `onClick` to the button below and call the `increment` function. */}
+        {/* 🌟 [02] - Add an `onClick` to the button below and call the `increment` function. */}
         <button className={styles.btn}>+</button>
       </div>
     </>
   );
 }
 
-/* 🌟 [05] - FINISHED! 💯  When clicking the buttons, the counter should decrement or increment. 💯 */
+/* 🌟 [06] - FINISHED! 💯  When clicking the buttons, the counter should decrement or increment. 💯 */
