359a9f9db8b5103d78626344e6283d848961f794
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-01-25 22:46:13 +0100

    .

--- src/exercises/08-from-class-to-useEffect/08.js
+++ src/exercises/08-from-class-to-useEffect/08.js
@@ -10,7 +10,39 @@ This is still under construction...
 /* 🌟 [00] - In this exercise we'll convert a class component
       with side effects to a function component with useEffect. */
 
-export default function App() {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("keypress", this.keyPressed);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keypress", this.keyPressed);
+  }
+
+  keyPressed = (event) => {
+    console.log(event.keyCode);
+    this.setState({ ...this.state, counter: this.state.counter + 1 });
+  };
+
+  componentDidUpdate() {
+    console.log("render...");
+  }
+
+  render() {
+    return <>{this.state.counter}This is a CLASS component... 🤮 </>;
+  }
+}
+
+/* 🌟 [xx] - Remove the export default from the class component and it to the function component below: */
+
+function App2() {
   return (
     <>
       <h1>Exercise 8</h1>
